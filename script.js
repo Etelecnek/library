d3f0dd3f273d3c527bfe1512ef7fba92859ea8e3
Etelecnek <126229297+Etelecnek@users.noreply.github.com>
2023-05-25 00:00:39 -0700

    Updated Javascript with buttons need to make button work

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-let readBool = "false";
-let numberedID = 1;
+
 
 function addbook() {
     document.getElementById("addnewbook").style.display="block";
 }
 
-function book(title, author, pages) {
-    this.title = title
-    this.author = author
-    this.pages = pages
+const book = {
+
 }
 
 function submitText() {
@@ -17,26 +14,32 @@ function submitText() {
     
 }
 
+function setVariables() {
+
+}
+
 function createBook() {
     const newbook = document.createElement('div')
     newbook.setAttribute('class', 'createdBook')
     const title = document.createElement('p')
     const author = document.createElement('p')
     const pages = document.createElement('p')
+    const readBtn = document.createElement('button')
+    readBtn.setAttribute('id', 'readbutt')
+    const removeBtn = document.createElement('button')
+    removeBtn.setAttribute('id', 'remove')
+    removeBtn.textContent = 'Remove'
     document.getElementById('body').appendChild(newbook)
     newbook.appendChild(title)
     newbook.appendChild(author)
     newbook.appendChild(pages)
+    newbook.appendChild(readBtn)
+    newbook.appendChild(removeBtn)
     title.innerHTML = document.getElementById('title').value 
     author.innerHTML = document.getElementById('author').value
     pages.innerHTML = document.getElementById('pages').value
-}
-
-function readCheckbox() {
-    var checkRead = document.getElementById('checkRead');
-    if (checkRead.checked == true) {
-        readBool = "true";
-    } else {
-        readBool = "false";
-    }
+    document.getElementById("addnewbook").style.display="none";
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('pages').value = "";
 }
